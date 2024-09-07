e51e806fe4c11918c4a3857824b35b4bdd50cf78
Harsh <harshkumarshaw09@gamil.com>
2024-09-07 14:13:17 +0530

    body-parser added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
  const express = require("express");
+ const bodyparser=require('body-parser');
  
  const {PORT} =require('./config/serverconfig');
 
@@ -6,6 +7,10 @@
 
     //create the express object
     const app=express();
+
+    app.use(bodyparser.json());
+    app.use(bodyparser.urlencoded({extended: true}));
+
     app.listen(PORT,()=>{
         console.log(`Server started at ${PORT}`); 
     });
