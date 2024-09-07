c105e9b059194dc9919e45d91a3757fae68b3f7a
Harsh <harshkumarshaw09@gamil.com>
2024-09-07 13:55:03 +0530

    setup basic env file

---
+++ src/index.js
@@ -0,0 +1,15 @@
+ const express = require("express");
+ 
+ const {PORT} =require('./config/serverconfig');
+
+ const setupandstartserver=async () =>{
+
+    //create the express object
+    const app=express();
+    app.listen(PORT,()=>{
+        console.log(`Server started at ${PORT}`); 
+    });
+ 
+ }
+
+ setupandstartserver();
