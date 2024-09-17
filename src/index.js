be4c15bd6fe47268fe76e72fe20b600c03a7bf7c
Harsh <harshkumarshaw09@gamil.com>
2024-09-17 22:10:38 +0530

    created the Airplane model and seeded value into it

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@
  const {PORT} =require('./config/serverconfig');
  const ApiRoutes=require('./routes/index');
 
+ const db=require('./models/index');
+ //const {Airplane}=require('./models/index');
+
  const setupandstartserver=async () =>{
 
     //create the express object
@@ -15,7 +18,7 @@
     app.use('/api', ApiRoutes);
 
 
-    app.listen(PORT,()=>{
+    app.listen(PORT,async()=>{
         console.log(`Server started at ${PORT}`); 
         if(process.env.SYNC_DB) {
             db.sequelize.sync({alter: true});
--- src/models/city.js
+++ src/models/city.js
@@ -1,30 +1,42 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class City extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-   static associate(models) {
-      // define association here
-     this.hasMany(models.Airport, {
-        foreignKey: 'cityId'
-      });
-    }
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Airports', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      cityId: {
+        type: Sequelize.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Cities',
+          key: 'id',
+          as: 'cityId'
+        },
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Airports');
   }
-  City.init({
-    name: {
-      type: DataTypes.STRING, 
-      allowNull: false,
-      unique: true
-    },
-  }, {
-    sequelize,
-    modelName: 'City',
-  });
-  return City;
 };
